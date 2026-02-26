0125d35a6db23b519f8124cabcacdc6c7267e33a
PemiduHerath <pemiduherath24176@gmail.com>
2026-02-27 00:02:23 +0530

    update itinerary

--- backend/itinerary-service/src/main/java/com/travelplan/itinerary/ItineraryServiceApplication.java
+++ backend/itinerary-service/src/main/java/com/travelplan/itinerary/ItineraryServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 @ComponentScan(basePackages = {"com.travelplan.itinerary", "com.travelplan.common"})
 public class ItineraryServiceApplication {
     public static void main(String[] args) {
