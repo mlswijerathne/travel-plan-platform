a8bfbdcb8c8bb19afd9137bf2ad63875fecb446a
jmbrathnayake <janithrathnayake01@gmail.com>
2026-02-28 03:25:00 +0530

    ticket tier  response dto

---
+++ backend/event-service/src/main/java/com/travelplan/event/dto/TicketTierResponse.java
@@ -0,0 +1,21 @@
+package com.travelplan.event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketTierResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer capacity;
+    private Integer availableSeats;
+}
